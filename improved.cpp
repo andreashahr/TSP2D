0cb6c4abb0639e6521d3e63348c5e53a21573dd8
Andreas Hahr <andreas.hahr@gmail.com>
2016-11-03 08:14:16 +0100

    getting wrong answer

--- improved.cpp
+++ improved.cpp
@@ -25,7 +25,7 @@ int distance(Location, Location);
 int** distanceMatrix;
 int* tour;
 int N;
-clock_t start;
+clock_t start, now;
 
 int main() {
   start = clock();
@@ -64,23 +64,31 @@ int main() {
   }
 
   greedyTour(locations);
+
   twoOpt();
 
   for(int i = 0; i < N; i++) {
     cout << tour[i] << "\n";
   }
-
   return 0;
 }
 
 // sets global var tour to greedy
 void greedyTour(Location* locations) {
   int from = 0; // start location
+  int maxdist = 0, maxdAt = 0;
+  for(int i = 0; i < N-1; i++) {
+    if(distanceMatrix[i][i+1] > maxdist) {
+      maxdist = distanceMatrix[i][i+1];
+      maxdAt = i+1;
+    }
+  }
+  from = maxdAt;
   tour[0] = from; // set start location
   for(int i = 1; i < N; i++) {
     locations[from].visited = true; // location is now visited
     int minDistance = 1000001; // smallest distance
-    int nextFrom = -1;
+    int nextFrom = 0;
     // find smallest distance
     for(int to = 0; to < N; to++) {
       if(locations[to].visited) continue;
@@ -104,28 +112,46 @@ int tourLength(int from, int to, int* atour) {
 
 void twoOpt() {
   int* newtour = new int[N]; // holds improved tour attempt
-  int best_distance = tourLength(0, N, tour); // beat current tour
-  int new_distance; // ...with new distance
+  int new_distance = tourLength(0, N, tour); // beat current tour
+  int edgediff, best_diff, tempM = 0;
   int* temptour; // for swapping tour <-> newtour ptrs
-  int optimize = N-1;
-  while(optimize) {
-    optimize--;
-    for(int i = 0; i < N-1; i++) {
-      for(int k = i+1; k < N; k++) {
+
+  for(int i = 1; i < N-1; i++) {
+    now = clock();
+    if((double) (now - start)/CLOCKS_PER_SEC > 1.98) return (void) 0;
+
+    for(int k = i+1; k < N-1; k++) {
+
+      edgediff =
+      distanceMatrix[tour[i-1]][tour[i]]+distanceMatrix[tour[k]][tour[k+1]] -
+      distanceMatrix[tour[i-1]][tour[k]]-distanceMatrix[tour[k+1]][tour[i]];
+
+      if(edgediff > 0) {
         twoOptSwap(newtour, i, k);
-        new_distance = tourLength(0, N, newtour);
-        if(new_distance < best_distance) {
-          best_distance = new_distance;
-          temptour = tour;
-          tour = newtour;
-          newtour = temptour;
-          optimize = N-1;
+        best_diff = edgediff;
+        tempM = k+1;
+        for(int m = k+1; m < N-2; m++) {
+          edgediff += distanceMatrix[newtour[k]][newtour[m+1]];
+          edgediff -= distanceMatrix[newtour[k]][newtour[m+2]];
+          if(edgediff <= 0) break;
+          if(edgediff > best_diff) {
+            best_diff = edgediff;
+            tempM = m;
+          }
         }
-      }
-      if((((double) clock()-(double)start)/(double)CLOCKS_PER_SEC) > 1.98) {
-        /*optimize = false;
-        break;*/
-        return (void) 0;
+        int loctmp = newtour[k+1];
+        while(tempM >= k) {
+          k++
+          newtour[k+1] = newtour[k+2];
+        }
+        newtour[k+2] = loctmp;
+
+        temptour = tour;
+        tour = newtour;
+        newtour = tour;
+
+        i = 0;
+        break;
       }
     }
   }
